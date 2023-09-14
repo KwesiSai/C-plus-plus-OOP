3bf942564fed21c008927666ff47752780e152d3
KwesiSai <kwesiabraham4503@gmail.com>
2023-09-14 13:27:00 +0000

    updateassetregistry.cpp

--- codechallenges/assetregistry.cpp
+++ codechallenges/assetregistry.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
- class Asset{
- private:
+
+class Asset {
+private:
     int serialId;
     string assetname;
     string owner;
@@ -10,39 +12,81 @@ using namespace std;
     bool isObsolete;
     Asset* next;
 
- public:
-     Asset(int id,const string& name, const string& owner, const string& department,bool obsolete)
-         : serialId(id),assetname(name), owner(owner), department(department), isObsolete(obsolete), next(nullptr){}
-     void display(){
-       cout<<"Asset serial ID is "<<serialId<<endl;
-       cout<<"The asset is a "<<assetname<<endl;
-       cout<<"Asset belongs to "<<owner<<endl;
-       cout<<"Asset is used in the "<<department<<" department."<<endl;
-       cout<<"Is Asset obsolete ? "<<(isObsolete ? " Yes":" No")<<endl;
-     }
-     int getSerialId() const { return serialId; }
+public:
+    Asset(int id, const string& name, const string& owner, const string& department, bool obsolete)
+        : serialId(id), assetname(name), owner(owner), department(department), isObsolete(obsolete), next(nullptr) {}
+
+    void display() {
+        cout << "Asset serial ID is " << serialId << endl;
+        cout << "The asset is a " << assetname << endl;
+        cout << "Asset belongs to " << owner << endl;
+        cout << "Asset is used in the " << department << " department." << endl;
+        cout << "Is Asset obsolete ? " << (isObsolete ? " Yes" : " No") << endl;
+    }
+
+    int getSerialId() const { return serialId; }
     void setIsObsolete(bool obsolete) { isObsolete = obsolete; }
-     Asset* getNext() const { return next; }
+    Asset* getNext() const { return next; }
     void setNext(Asset* asset) { next = asset; }
 
- };
+};
 
-int main()
-{
-        // Create a linked list to store assets
+// Function to create a new asset and return a pointer to it
+Asset* createAsset() {
+    int id;
+    string name, owner, department;
+    bool obsolete;
+
+    cout << "Enter asset serial ID: ";
+    cin >> id;
+
+    cout << "Enter asset name: ";
+    cin.ignore(); // Consume newline character
+    getline(cin, name);
+
+    cout << "Enter asset owner: ";
+    getline(cin, owner);
+
+    cout << "Enter asset department: ";
+    getline(cin, department);
+
+    cout << "Is asset obsolete (0 for No, 1 for Yes): ";
+    cin >> obsolete;
+
+    return new Asset(id, name, owner, department, obsolete);
+}
+
+int main() {
+    // Create a linked list to store assets
     Asset* head = nullptr;
 
-    // Add assets to the list
-    head = new Asset(1, "Keyboard", "John Doe", "IT", false);
-    head->setNext(new Asset(2,"Printer", "Jane Smith", "Finance", true));
-    head->getNext()->setNext(new Asset(3, "Router", "Bob Johnson", "HR", true));
+    char addMore;
 
+    do {
+        // Create a new asset and add it to the list
+        Asset* newAsset = createAsset();
+        newAsset->setNext(head);
+        head = newAsset;
+
+        cout << "Do you want to add more assets? (y/n): ";
+        cin >> addMore;
+    } while (addMore == 'y' || addMore == 'Y');
+       system("cls");
     // Display assets
     Asset* current = head;
     while (current != nullptr) {
         current->display();
-        std::cout << std::endl;
+        cout << endl;
         current = current->getNext();
     }
 
+    // Clean up memory
+    current = head;
+    while (current != nullptr) {
+        Asset* temp = current;
+        current = current->getNext();
+        delete temp;
+    }
+
+    return 0;
 }
